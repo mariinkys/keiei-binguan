2b9cc7dd6e20c280dc2c13930b1343879d4a9bdb
Alex Marín <kysgeneral.nrgua@slmail.me>
2023-11-30 17:43:16 +0100

    removed unused import

--- components/reservations/ReservationEditComponent.vue
+++ components/reservations/ReservationEditComponent.vue
@@ -36,7 +36,6 @@
 <script lang="ts">
 import { initDefaultReservation, mapPrismaReservationModel } from '@/server/models/reservationModel';
 import axios from 'axios'
-import moment from 'moment'
 
 export default {
    props: {
